9d9c119c7a6c58ffe7e77a99e0c52eebda7f44a4
yuzhiyizhan <liangritian18666@163.com>
2020-04-12 13:15:40 +0800

    修复日志重复打印的bug

--- Testapi/Commom/log.py
+++ Testapi/Commom/log.py
@@ -1,17 +1,17 @@
-import logging
 import os
 import time
 from Testapi.config.config import LOG_PATH
 from Testapi.config.config import LOG_NAME
 from loguru import logger
 
+if not os.path.exists(LOG_PATH):
+    os.mkdir(LOG_PATH)
+logger.add(LOG_NAME, level='INFO', rotation="10 MB", retention="7 days", backtrace=True, diagnose=True,
+           enqueue=True,
+           serialize=False)
+
 
 def Log(log):
-    if not os.path.exists(LOG_PATH):
-        os.mkdir(LOG_PATH)
-    logger.add(
-        LOG_NAME, level='INFO', rotation='10 MB'
-    )
-    logger.info(f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}{log}')
+    logger.info(log)
     # logging.basicConfig(filename=LOG_NAME, level=logging.INFO)
     # logging.info(f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}{log}')
